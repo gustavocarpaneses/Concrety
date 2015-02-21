b3008098533eb385cebfb7cba530ca4c174ed326
Gustavo Carpaneses <gcarpane@gmail.com>
2015-02-20 23:00:21 -0200

    Revert "Modificação para que todos JS e CSS sejam minifieds"
    This reverts commit 329a6c51ec3d7fb1c19f8a0ffccc7b7454d2d6af.

--- Concrety.Web/app/controllers/diarioObraController.js
+++ Concrety.Web/app/controllers/diarioObraController.js
@@ -1,8 +1,9 @@
 ﻿'use strict';
 app.controller('diarioObraController', function ($scope, $q, $http, diariosObraService, condicoesClimaticasService, accountService) {
+
     $scope.mensagem = "";
     $scope.empreendimentoAtual = accountService.empreendimentoAtual;
-    
+
     $scope.condicoesClimaticas = [];
     $scope.dropDownCondicoesClimaticasOptions = [];
 
