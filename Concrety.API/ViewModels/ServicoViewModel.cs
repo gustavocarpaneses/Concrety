42e22ed9be4e25997b079248ee2c76bea8ac62aa
Gustavo Carpaneses <gcarpane@gmail.com>
2018-05-31 17:12:31 -0300

    Normas em formato embed

--- Concrety.API/ViewModels/ServicoViewModel.cs
+++ Concrety.API/ViewModels/ServicoViewModel.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Globalization;
+
 namespace Concrety.API.ViewModels
 {
     public class ServicoViewModel
@@ -7,6 +9,16 @@ namespace Concrety.API.ViewModels
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public string Norma { get; set; }
+        public bool NormaIsAnUrl
+        {
+            get
+            {
+                return
+                    !string.IsNullOrWhiteSpace(Norma)
+                    &&
+                    Norma.StartsWith("http", true, CultureInfo.InvariantCulture);
+            }
+        }
         public bool Atual { get; set; }
         public bool Desabilitado { get; set; }
     }
