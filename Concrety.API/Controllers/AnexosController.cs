e21d01818df2a550db0cee8f76c9c03aa53126b7
Gustavo Carpaneses <gcarpane@gmail.com>
2018-07-21 15:42:15 -0300

    Retirando try/catch

--- Concrety.API/Controllers/AnexosController.cs
+++ Concrety.API/Controllers/AnexosController.cs
@@ -1,17 +1,11 @@
 ﻿using AutoMapper;
 using Concrety.API.ViewModels;
 using Concrety.Core.Entities;
-using Concrety.Core.Entities.Enumerators;
-using Concrety.Core.Extensions;
 using Concrety.Core.Interfaces.Services;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
 
 namespace Concrety.API.Controllers
@@ -37,35 +31,26 @@ namespace Concrety.API.Controllers
                 throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
             }
 
-            try
+            var anexo = new Anexo
             {
+                IdObra = idObra
+            };
 
-                var anexo = new Anexo
-                {
-                    IdObra = idObra
-                };
-
-                var provider = new MultipartFormDataStreamProvider(Path.GetTempPath());
+            var provider = new MultipartFormDataStreamProvider(Path.GetTempPath());
                             
-                await Request.Content.ReadAsMultipartAsync(provider).ConfigureAwait(false);
+            await Request.Content.ReadAsMultipartAsync(provider).ConfigureAwait(false);
 
-                foreach (MultipartFileData file in provider.FileData)
-                {
-                    anexo.NomeArquivoUpload = file.LocalFileName;
-                    anexo.Tipo = file.Headers.ContentType.MediaType;
-                    await _anexoService.CriarAsync(anexo).ConfigureAwait(false);
-                    File.Delete(file.LocalFileName);
-                }
-
-                var anexoViewModel = Mapper.Map<Anexo, AnexoViewModel>(anexo);
-
-                return Ok(anexoViewModel);
-            }
-            catch (Exception e)
+            foreach (MultipartFileData file in provider.FileData)
             {
-                return InternalServerError(e);
+                anexo.NomeArquivoUpload = file.LocalFileName;
+                anexo.Tipo = file.Headers.ContentType.MediaType;
+                await _anexoService.CriarAsync(anexo).ConfigureAwait(false);
+                File.Delete(file.LocalFileName);
             }
+
+            var anexoViewModel = Mapper.Map<Anexo, AnexoViewModel>(anexo);
+
+            return Ok(anexoViewModel);
         }
-        
     }
 }
